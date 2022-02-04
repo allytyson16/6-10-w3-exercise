50ebe851e86cefdc70f396219527c841ad5c9013
Adelaide <88384474+allytyson16@users.noreply.github.com>
2022-02-04 19:36:39 +0000

    Update Question 10.cs

--- Question 10.cs
+++ Question 10.cs
@@ -6,7 +6,21 @@ namespace Udemyyyyy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+//10. Write a C# Sharp program to that takes three numbers(x,y,z) as input and print the output of (x+y).z and x.y + y.z
+			Console.Write("Enter first number - ");
+			double x = double.Parse(Console.ReadLine());
+
+			Console.Write("Enter second number - ");
+			double y = double.Parse(Console.ReadLine());
+
+			Console.Write("Enter third number - ");
+			double z = double.Parse(Console.ReadLine());
+
+			double output1 = (x + y) * z;
+			double output2 = x * y + y * z;
+
+			Console.WriteLine("The output of (first number ({0}) + second number ({1})) * third number ({2}) is ({3}).", x, y, z, output1);
+			Console.WriteLine("The putput of first number ({0}) * second number ({1}) + second number ({2}) * third number ({3}) is ({4}).", x, y, y, z, output2);
         }
     }
 }
