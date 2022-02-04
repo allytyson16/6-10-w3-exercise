ab9bb2d6c8011141607b66821e81c661515e2ff6
Adelaide <88384474+allytyson16@users.noreply.github.com>
2022-02-04 19:09:31 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,28 @@ namespace Udemyyyyy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+//7. Write a C# Sharp program to print on screen the output of adding, subtracting, multiplying and dividing of two numbers which will be entered by the user.
+
+					Console.Write("Enter first number - ");
+					int fNumber = int.Parse(Console.ReadLine());
+					Console.Write("Enter second number - ");
+					int sNumber = int.Parse(Console.ReadLine());
+
+				//for adding
+					int add = fNumber + sNumber;
+					Console.WriteLine("The sum of the numbers are " + add);
+
+				//for subtraction
+					int difference = fNumber - sNumber;
+					Console.WriteLine("The difference of the numbers are " + difference);
+
+				//for multiplication
+					int multiplication = fNumber - sNumber;
+					Console.WriteLine("The product of the numbers are " + multiplication);
+
+				//for division
+					int division = fNumber - sNumber;
+					Console.WriteLine("The answer for dividing the two numbers are " + division);
+			        }
     }
 }
