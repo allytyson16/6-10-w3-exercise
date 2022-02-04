b294d794e4d03c1d6d916e4a3b5345358ae3031f
Adelaide <88384474+allytyson16@users.noreply.github.com>
2022-02-04 19:31:05 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace Udemyyyyy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //9. Write a C# Sharp program that takes four numbers as input to calculate and print the average.
+
+			Console.Write("Enter your first prefered number - ");
+			double firstNumber = double.Parse(Console.ReadLine());
+
+			Console.Write("Enter your second prefered number - ");
+			double secondNumber = double.Parse(Console.ReadLine());
+
+			Console.Write("Enter your third prefered number - ");
+			double thirdNumber = double.Parse(Console.ReadLine());
+
+			Console.Write("Enter your fourth prefered number - ");
+			double fourthNumber = double.Parse(Console.ReadLine());
+
+			double average = (firstNumber + secondNumber + thirdNumber + fourthNumber) / 4;
+			Console.WriteLine("The average for the four numbers you inputted is - {0}", average);
+
         }
     }
 }
